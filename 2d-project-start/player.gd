e27ac2a5a5dacc690fa8e8403ab48a89c13da6a4
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-08-05 10:51:30 +0200

    add health bar and damage

--- 2d-project-start/player.gd
+++ 2d-project-start/player.gd
@@ -1,26 +1,28 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health:float = 100.0
+
+
 @export var happy_boo:Node2D
 const SPEED:float = 600.0
-const JUMP_VELOCITY:float = -400.0
+const DAMAGE_RATE:float = 5.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity:float = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-func _physics_process(_delta:float):
-	'''
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	'''
+func _physics_process(delta:float):
 	#direction
 	var direction = Input.get_vector("move_left", "move_right","move_up","move_down")
 	velocity = direction * SPEED
-		
 	move_and_slide()
+	
+	
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
+	
 func _process(_delta):
 	#animation
 	if velocity.length() > 0.0:
