5c74c639efe3f5a765925698db1052f1859a3e97
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-08-05 10:35:27 +0200

    added bullets and damage

--- 2d-project-start/gun.gd
+++ 2d-project-start/gun.gd
@@ -3,6 +3,8 @@ extends Area2D
 @onready var static_position:float = rotation
 var reset_speed:float = 2.0
 
+
+
 func _physics_process(delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
@@ -16,3 +18,15 @@ func reset_position(delta):
 			rotation = static_position
 		else:
 			rotation = lerp_angle(rotation, static_position, reset_speed*delta)
+			
+func shoot():
+	const BULLET = preload("res://bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	
+	%ShootingPoint.add_child(new_bullet)
+
+
+func _on_timer_timeout():
+	shoot()
--- 2d-project-start/slime.gd
+++ 2d-project-start/slime.gd
@@ -1,16 +1,22 @@
 extends CharacterBody2D
 #@export var slime:Node2D
+var health = 3
+
+
 @onready var player = get_node("/root/Game/Player")
-@onready var Slime = get_node("Slime")
+@onready var Slime = %Slime
+
+func _ready():
+	Slime.play_walk()
+
+
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * 300
 	move_and_slide()
 
-func _process(_delta):
-	#animation
-	if velocity.length() > 0.0:
-		Slime.play_walk()
-	else:
-		Slime.play_hurt()
-
+func take_damage():
+	health -= 1
+	Slime.play_hurt()
+	if health <= 0:
+		queue_free()
