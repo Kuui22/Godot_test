6e132ef7a8e060fb7b81b809bfcfd628c7f4a9ae
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-08-05 14:30:28 +0200

    isometric example
    https://www.youtube.com/watch?v=dclc8w6JW7Y

---
+++ isometric/TileMap.gd
@@ -0,0 +1,19 @@
+extends TileMap
+
+const green_block_atlas_pos = Vector2i(2,0)
+const blue_block_atlas_pos = Vector2i(0,0)
+const main_source = 0
+
+enum layers{
+	level0 = 0,
+	level1 = 1,
+	level2 = 2,
+	}
+
+
+func _ready():
+	for y in range(3):
+		for x in range(3):
+			set_cell(layers.level0, Vector2i(2 + x, 2 + y), main_source, green_block_atlas_pos)
+			
+	set_cell(layers.level1, Vector2i(2, 2), main_source, blue_block_atlas_pos)
