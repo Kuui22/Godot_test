9c082bba9b347602cba20aadb82cb79b987c598c
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-08-05 14:58:18 +0200

    movement and collision
    https://www.youtube.com/watch?v=8HvcHtauKoc

--- isometric/TileMap.gd
+++ isometric/TileMap.gd
@@ -1,7 +1,8 @@
 extends TileMap
 
-const green_block_atlas_pos = Vector2i(2,0)
-const blue_block_atlas_pos = Vector2i(0,0)
+const green_block_atlas_pos = Vector2i(2,0) #level 0 floor
+const white_block_atlas_pos = Vector2i(3,0) #level 1 walls
+const boundary_block_atlas_pos = Vector2i(0,1) #invisible walls
 const main_source = 0
 
 enum layers{
@@ -10,10 +11,29 @@ enum layers{
 	level2 = 2,
 	}
 
-
 func _ready():
-	for y in range(3):
-		for x in range(3):
-			set_cell(layers.level0, Vector2i(2 + x, 2 + y), main_source, green_block_atlas_pos)
+	place_platform()
+	place_boundaries()
+
+func place_platform():
+	for y in range(9):
+		for x in range(9):
+			set_cell(layers.level0, Vector2i(-1 + x, -1 + y), main_source, green_block_atlas_pos)
 			
-	set_cell(layers.level1, Vector2i(2, 2), main_source, blue_block_atlas_pos)
+	set_cell(layers.level1, Vector2i(2, 2), main_source, white_block_atlas_pos)
+
+func place_boundaries():
+	var offsets = [
+		Vector2i(0,-1),
+		Vector2i(0,1),
+		Vector2i(1,0),
+		Vector2i(-1,0),
+	]
+	#get all cells used on floor
+	var used = get_used_cells(layers.level0)
+	for spot in used:
+		for offset in offsets:
+			var current_spot = spot + offset
+			if get_cell_source_id(layers.level0, current_spot) == -1: #if empty
+				set_cell(layers.level0, current_spot, main_source, boundary_block_atlas_pos)
+				
