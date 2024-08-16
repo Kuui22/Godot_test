c8ea06eee97ac470366bc1df305f005b26162941
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-08-16 12:57:00 +0200

    dio merda
    fanculo limbo ai

--- 2d-project-start/mobs/slime.gd
+++ 2d-project-start/mobs/slime.gd
@@ -1,24 +1,39 @@
 extends CharacterBody2D
 #@export var slime:Node2D
-var health:int = 3
-var experience:int = 10
+
+#signals
+
 signal dead(data:String,experience:int,scene)
 
+#onready and consts
 @onready var player = get_node("/root/Game/Player")
+#get_tree().get_nodes_in_group("player")
 @onready var Slime = %Slime
-@export var DAMAGE:float = 5.0
-#@export var drops:inv_item 
-
 const PICKUP_SCENE = preload("res://items/pickup.tscn")
 const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
 const mob_name:String = "Slime"
+const SPEED:float = 300
 
+#stats
 
+var health:int = 3
+var experience:int = 10
+@export var DAMAGE:float = 5.0
+
+#loot
 var loot_table
 
+#status
+
 var target
-const SPEED:float = 300
-const SPEEDFACTOR:float = 50
+@export var has_target:bool = false
+var status = "RUNNING"
+var current_idle_target_position:Vector2 = update_random_position()
+@export var min_range:float = 400.0
+@export var max_range:float = 1000.0
+
+
+
 
 func movetotarget(targ,speed):
 	var direction = global_position.direction_to(targ.global_position)
@@ -37,7 +52,11 @@ func _physics_process(_delta):
 	if(target):
 		movetotarget(target,SPEED)
 	else:
-		pass
+		if(status == "RUNNING"):
+			status_check()
+		else:
+			current_idle_target_position = update_random_position()
+			status = "RUNNING"
 	move_and_slide()
 
 #getting hit
@@ -78,3 +97,25 @@ func possible_drops():
 			drop = ItemDB.get_item(int(i[0]))
 			return drop
 	return drop
+
+
+@export var speed:float = 300.0
+@export var tolerance:float = 10.0
+
+func status_check():
+	var target_pos:Vector2 = current_idle_target_position
+	var distance:float = global_position.distance_to(target_pos)
+	
+	if distance < tolerance:
+		move(target_pos,0)
+		status = "FINISHED"
+		print("FINISHED")
+	
+	else:
+		move(target_pos,speed)
+		status = "RUNNING"
+		
+func update_random_position():
+	var movement_vector:Vector2 = Vector2(randf_range(-min_range, max_range), randf_range(-min_range, max_range))
+	var final_position:Vector2 = global_position + movement_vector
+	return final_position
