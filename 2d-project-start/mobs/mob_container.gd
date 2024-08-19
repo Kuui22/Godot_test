db13421f6cddb2f87441199e190a3c3faeaa57e4
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2024-08-19 12:47:57 +0200

    better scalability for mob_container
    hell ye

--- 2d-project-start/mobs/mob_container.gd
+++ 2d-project-start/mobs/mob_container.gd
@@ -1,40 +1,44 @@
 extends Node
 
-
-const biome_1_mobs:Dictionary = {
-	"slime" : preload("res://mobs/slime.tscn")
+#dictionary containing a dictionary for each biome (numbered)
+const BIOME_MOBS: Dictionary = {
+	1:{
+		"slime" : preload("res://mobs/slime.tscn")
+	}
 }
+#variable to get random mobs
+var biome_mob_arrays: Dictionary = {}
 
-var biome1array:Array = []
 
 func _ready() -> void:
-	for key in biome_1_mobs:
-		biome1array.append(biome_1_mobs[key])
+	#arrange biomes in array for random selection
+	for biome in BIOME_MOBS:
+		biome_mob_arrays[biome] = BIOME_MOBS[biome].values()
 
 
 func spawn_mob(biome:int,mob=null):
-	var selected_mob
+	var selected_mob:PackedScene
 	if mob:
 		selected_mob = get_selected_mob(biome,mob)
 	else:
 		selected_mob = get_random_mob(biome)
-			
-	var new_mob = selected_mob.instantiate()
-	return new_mob
-
-func get_selected_mob(biome:int,mob:String):
-	var selected_mob
-	match biome:
-		1:
-			selected_mob = biome_1_mobs[mob]
-	return selected_mob
-
-func get_random_mob(biome:int):
-	var selected_mob
-	var selector
-	match biome:
-		1:
-			selector = randi_range(0,biome1array.size()-1)
-			selected_mob = biome1array[selector]
-	return selected_mob
 	
+	if selected_mob:
+		var new_mob = selected_mob.instantiate()
+		return new_mob
+	else:
+		push_warning("No mob found for biome "+str(biome) + "and mob type" +str(mob))
+		return null
+
+func get_selected_mob(biome: int, mob: String) -> PackedScene:
+	if biome in BIOME_MOBS and mob in BIOME_MOBS[biome]:
+		return BIOME_MOBS[biome][mob]
+	return null
+
+func get_random_mob(biome: int) -> PackedScene:
+	if biome in biome_mob_arrays and not biome_mob_arrays[biome].is_empty():
+		return biome_mob_arrays[biome].pick_random()
+	return null
+
+func get_biome_mobs(biome: int) -> Array:
+	return biome_mob_arrays.get(biome, [])
